7d4a6ca664dc072470b68f6ca675715bedaca10b
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2026-03-26 11:49:13 +0200

    feat:Init app structure

---
+++ WhatDidIWrite/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Hosting;
+using RazorConsole.Core;
+using WhatDidIWrite;
+
+IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args)
+    .UseRazorConsole<App>();
+IHost host = hostBuilder.Build();
+await host.RunAsync();
